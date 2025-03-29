43631c674beff63259663f9b1837314e22922179
amareshka <amareshbevoor@gmail.com>
2025-03-29 14:15:53 +0530

    Gridlight implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,77 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-
-
+function Cell({ filled, onClick, isDisabled, label }) {
   return (
-    <h2>Welcome to Grid light</h2>
-  )
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
 }
 
-export default App
+export default function App() {
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+
+  const config = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const timer = setInterval(() => {
+      setOrder((origOrder) => {
+        const newOrder = origOrder.slice();
+        newOrder.pop();
+
+        if (newOrder.length === 0) {
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+
+        return newOrder;
+      });
+    }, 300);
+  };
+
+  const activateCells = (index) => {
+    const newOrder = [...order, index];
+    setOrder(newOrder);
+    // deactivate
+    if (newOrder.length === config.flat(1).filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <h2>Welcome to Grid Light</h2>
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: `repeat(${config[0].length}, 1fr)`,
+        }}
+      >
+        {config.flat(1).map((value, index) => {
+          return value ? (
+            <Cell
+              key={index}
+              label={`Cell ${index}`}
+              filled={order.includes(index)}
+              onClick={() => activateCells(index)}
+              isDisabled={order.includes(index) || isDeactivating}
+            />
+          ) : (
+            <span />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
